6aacf2c62b5d438041786662d5acd77479787a6e
Martin Vila <vilamartin21@gmail.com>
2021-09-27 21:15:29 -0300

    lectura_datos

--- src/Primer_parte.java
+++ src/Primer_parte.java
@@ -1,8 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Primer_parte {
 
+	private static String datos;
+	
 	public static void main(String[] args) {
-		System.out.print("5");
+		
+		try {
+			  
+			BufferedReader in = new BufferedReader(new FileReader("anexo1.txt"));
+			
+            datos = in.readLine();
+            
+            /*while(datos !=  null) {
+            	List<String> tokens =  Arrays.asList(cadena.split("-"));
+            	nroDni = tokens.get(0).trim();
+            	if(nroDni.equals(dni)) {
+            		nombre = tokens.get(1).trim();
+            		categoria = tokens.get(2).trim();
+            		
+            		cliente = ClienteFactory.getInstance().getCliente(dni, nombre, categoria);
+            	}
+            	
+            	datos = in.readLine();
+            }*/
+            
+            in.close();
+            
+        }
+        catch (IOException e) {
+            System.out.println("excepcion IO" + e);
+        }
+		
+		System.out.println("Codigo: " + datos);
+		
 	}
+	
+		
+		
 
 }
